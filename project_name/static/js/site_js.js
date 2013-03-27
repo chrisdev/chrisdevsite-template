a4306b51554538f56389c21cef942bd3db2fc7f7
Lendl Smith <lendl.smith@gmail.com>
2013-03-27 14:13:58 -0400

    new with reveal for frontendadmin

--- project_name/static/js/site_js.js
+++ project_name/static/js/site_js.js
@@ -1,4 +1,3 @@
-
 $('a.frontendadmin_edit').hover(
    function(){$(this).parent().css('outline', '4px solid #E9B007')},
    function(){$(this).parent().css('outline', '')}
@@ -6,14 +5,8 @@ $('a.frontendadmin_edit').hover(
 
 $('a.frontendadmin').click(function(event) {
             event.preventDefault();
-            var $div = $('<div>').addClass('reveal-modal').appendTo('body');
             $this = $(this);
             $.get($this.attr('href'), function(data) {
-              return $div.empty().html(data).append('<a class="close-reveal-modal">&#215;</a>').reveal();
+              $('#modalContent').html(data);
             });
-    });
-
-$(".fallback-flip").click(function(){
-   $("body").toggleClass("wf-inactive");
 });
-
